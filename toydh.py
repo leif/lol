6a173cd011c7eca24a34365a0cb0c1c642716fce
Leif Ryge <leif@synthesize.us>
2016-01-16 03:32:33 +0000

    toydh: test uniformdh-style "flip"

--- toydh.py
+++ toydh.py
@@ -25,6 +25,20 @@ F71C35FD AD44CFD2 D74F9208 BE258FF3 24943328 F67329C0
 FFFFFFFF FFFFFFFF
 """.replace(' ','').replace('\n','').replace('\t',''), 16)
 
+
+# 1536-bit prime from RFC3526 (via obfs3_dh.py)
+rfc3526_p = int("""
+FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+670C354E 4ABC9804 F1746C08 CA237327 FFFFFFFF FFFFFFFF""".replace(' ','').replace('\n','').replace('\t',''), 16)
+
+rfc3526_q = (rfc3526_p-1)/2
+
 # oakley2_p is a safe prime and oakley2_q is its sophie-germain prime
 assert oakley2_q*2 + 1 == oakley2_p
 
@@ -84,12 +98,22 @@ def test(g,p,q,a,b,z):
     # test blinded DH
     assert pow(A_,b,p) == pow(B,a_,p), (g,p,q,a,b,z)
 
+    # if both private keys are even, try fliping the public keys like
+    # obfs3_dh.UniformDH does (this also works with smaller-order generators
+    # so it does not mean the public keys are necessarily uniform)
+    if (not a % 2) and (not b % 2):
+        Au = p - A
+        Bu = p - B
+        assert pow(Au,b,p) == pow(B,a,p) ==\
+               pow(Bu,a,p) == pow(A,b,p), (g,p,q,a,b)
+
 def test_(p, q, gs, n):
     """
     >>> gs=generators(127,7)
     >>> test_(127, 7, generators(127, 7), 7)
     >>> test_(47, 23, generators(47, 23), 23)
     >>> test_(oakley2_p, oakley2_q, range(10), 10)
+    >>> test_(rfc3526_p, rfc3526_q, [2], 10)
     """
     for g in gs:
         for a in range(n):
